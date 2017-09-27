f1d06edbd1fab9a2e5c638d0b6c0026207d3e7df
suryakant1698 <suryakant.rocky@gmail.com>
2017-09-27 21:20:32 +0530

    option for sending mail to multiple recipiennts(WORKING)

--- userPages/composeEmail.aspx
+++ userPages/composeEmail.aspx
@@ -1,65 +1,48 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/userPages/userLoggedIn.master" AutoEventWireup="true" CodeFile="composeEmail.aspx.cs" Inherits="userPages_compose" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="mainContentPlaceHolder" runat="Server">
-
-
-    <asp:Repeater runat="server" ID="htmlTemplateDisplay"></asp:Repeater>
+    
     <div>
-
-
         <h1>Mail Sending Portion</h1>
-
-
-
-
-        <h3>Select Recipients</h3>
-
-        <div style="border-color:white; border:4px;">
-        <asp:Repeater ID="rptrCategory" runat="server">
-            <ItemTemplate>
-                <%#  Eval("categoryName")%>
-                <%# Eval("ID") %>
-                <table style="width: 600px; border: double solid; border-color: white">
-
-
-                    <tr>
-                        <td>
-                            <asp:Label Width="50%" ID="lblCategoryName" Text='<%# Eval("categoryName") %>' runat="server"></asp:Label></td>
-                        <td>
-                            <asp:CheckBoxList ID="cblRecipients" runat="server" DataSourceID="SqlDataSource1" DataTextField="name" DataValueField="ID"></asp:CheckBoxList>
-                            <asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString='<%$ ConnectionStrings:RegistrationConnectionString %>' SelectCommand="SELECT [ID], [name] FROM [tblRecipients] WHERE ([CategoryId] = @CategoryId)">
-                                <SelectParameters>
-                                    <asp:ControlParameter ControlID="hiddenfield" PropertyName="Value" Name="CategoryId"></asp:ControlParameter>
-                                </SelectParameters>
-                                <SelectParameters>
-                                </SelectParameters>
-
-                            </asp:SqlDataSource>
-                            <asp:HiddenField runat="server" ID="hiddenfield" Value='<%# Eval("ID") %>' />
-                        </td>
-                    </tr>
-                </table>
-            </ItemTemplate>
-        </asp:Repeater>
-            </div>
+       
+        <div style="border-color: white; border:4px; margin-bottom:40px; ">
+           <table style="width: 600px; border: double solid; margin-left:7%;  border-color: white; background-color:cadetblue;">
+               <caption><b>Select Recipients</b></caption>
+             <asp:Repeater ID="rptrCategory" runat="server">
+                <ItemTemplate>
+                    
+                    
+                        <tr>
+                            <td>
+                                <asp:Label Width="100%" ID="lblCategoryName" Text='<%# Eval("categoryName") %>' runat="server"></asp:Label></td>
+                            <td>
+                                <asp:CheckBoxList ID="cblRecipients" runat="server" DataSourceID="SqlDataSource1" DataTextField="name" DataValueField="ID" RepeatDirection="Horizontal" RepeatColumns="4"></asp:CheckBoxList>
+                                <asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString='<%$ ConnectionStrings:RegistrationConnectionString %>' SelectCommand="SELECT [ID], [name] FROM [tblRecipients] WHERE ([CategoryId] = @CategoryId)">
+                                    <SelectParameters>
+                                        <asp:ControlParameter ControlID="hiddenfield" PropertyName="Value" Name="CategoryId"></asp:ControlParameter>
+                                    </SelectParameters>
+                                    <SelectParameters>
+                                    </SelectParameters>
+                                </asp:SqlDataSource>
+                                <asp:HiddenField runat="server" ID="hiddenfield" Value='<%# Eval("ID") %>' />
+                            </td>
+                        </tr>
+                    
+                </ItemTemplate>
+            </asp:Repeater>
+            </table>
+        </div>
         <table style="width: 807px">
+            <caption><b>Enter Mail Credentials</b></caption>
             <tr>
-                <td style="width: 79px; text-align: right">
-                    <asp:Label Text="Email" runat="server" ID="lblRecipientEmail"></asp:Label></td>
-                <td style="width: auto">
-                    <asp:TextBox ID="tbxRecipientEmail" placeholder="Enter Recipient's email here" runat="server" Width="562px"></asp:TextBox></td>
-                <td style="width: 416px">
-                    <asp:RequiredFieldValidator ID="RFValidatorEmail" runat="server" ControlToValidate="tbxRecipientEmail" ErrorMessage="mandatory field" ValidationGroup="mailCredentials" ForeColor="Red"></asp:RequiredFieldValidator></td>
-            </tr>
-            <tr>
-                <td style="width: 79px; text-align: right">Subject</td>
+                <td style="width: 165px; text-align: right">Subject</td>
                 <td>
                     <asp:TextBox ID="tbxSubject" runat="server" placeholder="Enter subject here" Width="562px"></asp:TextBox></td>
                 <td>
                     <asp:RequiredFieldValidator ID="RFValidatorSubject" runat="server" ControlToValidate="tbxSubject" ErrorMessage="Mandatory Field" ValidationGroup="mailCredentials" ForeColor="Red"></asp:RequiredFieldValidator></td>
             </tr>
             <tr>
-                <td style="width: 79px; text-align: right">
+                <td style="width: 165px; text-align: right">
                     <asp:Label ID="lblMailBody" runat="server" Text="Body"></asp:Label></td>
                 <td>
                     <asp:TextBox TextMode="MultiLine" Height="200px" placeholder="Enter mail's body here" runat="server" ID="tbxMailBody" Width="562px"></asp:TextBox></td>
@@ -67,14 +50,14 @@
                     <asp:RequiredFieldValidator ID="RFValidatorbody" ErrorMessage="mandatory field" ForeColor="red" runat="server" ValidationGroup="mailCredentials" ControlToValidate="tbxMailBody"></asp:RequiredFieldValidator></td>
             </tr>
             <tr>
-                <td style="width: 79px; text-align: right">Password</td>
+                <td style="width: 165px; text-align: right">Password</td>
                 <td>
                     <asp:TextBox ID="tbxPassword" TextMode="Password" runat="server" placeholder="Enter your registered mail's password here" Width="561px"></asp:TextBox></td>
                 <td style="width: 416px">
                     <asp:RequiredFieldValidator ErrorMessage="Mandatory field" ForeColor="Red" ID="RFValidator" runat="server" ValidationGroup="mailCredentials" ControlToValidate="tbxPassword"></asp:RequiredFieldValidator></td>
             </tr>
             <tr>
-                <td style="text-align: right; width: 79px">Select Template</td>
+                <td style="text-align: right; width: 165px">Select Template</td>
                 <td>
                     <asp:DropDownList ID="ddlTemplateSelector" Width="100px" runat="server"></asp:DropDownList>
                 </td>
@@ -83,7 +66,7 @@
                 <td colspan="3" style="text-align: center">
                     <asp:Button ID="btnSend" Width="100px" Height="50px" Text="Send" runat="server" OnClick="btnSend_Click" /></td>
             </tr>
-        </table>
 
+        </table>
     </div>
 </asp:Content>
--- userPages/composeEmail.aspx.cs
+++ userPages/composeEmail.aspx.cs
@@ -10,22 +10,19 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 public partial class userPages_compose : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["ID"] == null)
-            Response.Redirect("../Login.aspx");
         if (!IsPostBack)
         {
             using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
             {
-                SqlCommand com = new SqlCommand("select ID,name from tblTemplates", con);
+                SqlCommand com = new SqlCommand("select filePath,name from tblTemplates", con);
                 con.Open();
                 ddlTemplateSelector.DataSource = com.ExecuteReader();
                 ddlTemplateSelector.DataTextField = "name";
-                ddlTemplateSelector.DataValueField = "ID";
+                ddlTemplateSelector.DataValueField = "filePath";
                 ddlTemplateSelector.DataBind();
             }
             using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
@@ -36,7 +33,6 @@ public partial class userPages_compose : System.Web.UI.Page
                 rptrCategory.DataSource = ds1;
                 rptrCategory.DataBind();
             }
-
         }
     }
     protected void btnSend_Click(object sender, EventArgs e)
@@ -44,40 +40,41 @@ public partial class userPages_compose : System.Web.UI.Page
         foreach (RepeaterItem i in rptrCategory.Items)
         {
             CheckBoxList cb = (CheckBoxList)i.FindControl("cblRecipients");
-
             foreach (ListItem li in cb.Items)
             {
                 if (li.Selected)
-                    sendEmail(li.Value);
+                {
+                    sendEmail(Convert.ToInt32(li.Value));
+                }
+
             }
+
         }
     }
-    protected void sendEmail(string recipientID)
+
+    protected void sendEmail(int recipientID)
     {
         using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
         {
             con.Open();
-            SqlCommand getTemplatePath = new SqlCommand("select filePath from tblTemplates where Id='" + ddlTemplateSelector.SelectedItem.Value + "'", con);//fetching template path of the selected item
-            string templatePath = getTemplatePath.ExecuteScalar().ToString();
             SqlCommand getRecipientName = new SqlCommand("select name from tblRecipients where ID='" + recipientID + "'", con);
             string recipientName = getRecipientName.ExecuteScalar().ToString();
             SqlCommand getRecipientEmail = new SqlCommand("select email from tblRecipients where ID='" + recipientID + "'", con);
-            string recipientEmail = getRecipientName.ExecuteScalar().ToString();
-            SqlCommand getUserEmail = new SqlCommand("select email from tblUsers where ID='" + Session["ID"].ToString() + "'", con);
+            string recipientEmail = getRecipientEmail.ExecuteScalar().ToString();
+            SqlCommand getUserEmail = new SqlCommand("select email from tblUsers where ID='" + Convert.ToInt32(Session["ID"]) + "'", con);
+            con.Close();
+            con.Open();
             string userEmail = getUserEmail.ExecuteScalar().ToString();
             string body = string.Empty;
-            using (StreamReader reader = new StreamReader(Server.MapPath(templatePath)))
+            using (StreamReader reader = new StreamReader(Server.MapPath(ddlTemplateSelector.SelectedItem.Value)))
             {
                 //changing the value of the placeholders in template as per recipient's information
                 body = reader.ReadToEnd();
                 body = body.Replace("{UserName}", recipientName);
                 body = body.Replace("{body}", tbxMailBody.Text);
             }
-            using (MailMessage mail = new MailMessage())
+            using (MailMessage mail = new MailMessage(userEmail, recipientEmail))
             {
-
-                //mail.From = userEmail;
-                //mail.To=recipientEmail;
                 mail.Subject = tbxSubject.Text;
                 mail.Body = body;
                 mail.IsBodyHtml = true;
@@ -93,74 +90,3 @@ public partial class userPages_compose : System.Web.UI.Page
         }
     }
 }
-
-//PREVIOUS CODE HAS BEEN PUT INTO COMMENTS
-
-
-//    protected void btnSend_Click(object sender, EventArgs e)
-//    {
-//        Page.Validate("mailCredentials");
-//        if (!Page.IsValid)
-//            return;
-//        string message;
-//        using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
-//        {
-
-//            con.Open();
-//            string templateID = ddlTemplateSelector.SelectedItem.Value;
-//            SqlCommand getTemplatePath = new SqlCommand("select filePath from tblTemplates where ID='" + templateID + "'", con);//fetching template path of the selected item
-//            string templatePath = getTemplatePath.ExecuteScalar().ToString();
-//            SqlCommand getRecpientID = new SqlCommand("select ID from tblRecipients where email='" + tbxRecipientEmail.Text + "'", con);
-//            int recipientID = Convert.ToInt32(getRecpientID.ExecuteScalar().ToString());
-//            SqlCommand checkEmail = new SqlCommand("select count(*) from tblRecipients where email='" + tbxRecipientEmail.Text + "'", con);//ensuring email of the recipient is already added
-//            int temp = Convert.ToInt32(checkEmail.ExecuteScalar().ToString());
-
-//            if (temp == 1)
-//            {
-//                SqlCommand selectName = new SqlCommand("select name from tblRecipients where email='" + tbxRecipientEmail.Text + "'", con);//fetching name from te
-//                string RecipientName = selectName.ExecuteScalar().ToString();
-
-//                string body = string.Empty;
-//                using (StreamReader reader = new StreamReader(Server.MapPath(templatePath)))
-//                {
-//                    //changing the value of the placeholders in template as per recipient's information
-//                    body = reader.ReadToEnd();
-//                    body = body.Replace("{UserName}", RecipientName);
-//                    body = body.Replace("{body}", tbxMailBody.Text);
-//                }
-//                SqlCommand getSenderEmail = new SqlCommand("select email from tblUsers where ID='" + Session["ID"].ToString() + "'", con);
-//                string senderEmail = getSenderEmail.ExecuteScalar().ToString();
-//                sendEmail(senderEmail, body);
-//                SqlCommand insertMail = new SqlCommand("insert into tblSentMails(body,RecipientID,templateID) values(@body,@recipientID,@templateID)", con);
-//                insertMail.Parameters.AddWithValue("@body", tbxMailBody.Text);//storing the sent mail in database
-//                insertMail.Parameters.AddWithValue("@recipientID", recipientID);
-//                insertMail.Parameters.AddWithValue("@templateID", Convert.ToInt32(templateID));
-//                insertMail.ExecuteNonQuery();
-//                message = "mail  was sent succesfuly";
-
-//            }
-//            else message = "Recipient's email Invalid check again or add a new one";
-//            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('" + message + "');", true);
-//        }
-
-//    }
-//    protected void sendEmail(string senderEmail, string body)
-//    {
-//        using (MailMessage mail = new MailMessage(senderEmail, tbxRecipientEmail.Text))
-//        {
-//            mail.Subject = tbxSubject.Text;
-//            mail.Body = body;
-//            mail.IsBodyHtml = true;
-//            SmtpClient smtp = new SmtpClient();
-//            smtp.Host = "smtp.gmail.com";
-//            smtp.EnableSsl = true;
-//            NetworkCredential NetworkCred = new NetworkCredential(senderEmail, tbxPassword.Text);
-//            smtp.UseDefaultCredentials = true;
-//            smtp.Credentials = NetworkCred;
-//            smtp.Port = 587;
-//            smtp.Send(mail);
-
-//        }
-//    }
-//}
-
