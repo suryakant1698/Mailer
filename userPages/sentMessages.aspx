4dd4210dcba4412f74d742c2259532722670c1d0
suryakant1698 <suryakant.rocky@gmail.com>
2017-10-01 03:21:31 +0530

    showing all the sent mails of user

---
+++ userPages/sentMessages.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/userPages/userLoggedIn.master" AutoEventWireup="true" CodeFile="sentMessages.aspx.cs" Inherits="userPages_sentMessages" %>
+
+
+
+    <asp:Content ID="Content1" ContentPlaceHolderID="mainContentPlaceHolder" Runat="Server">    
+
+    <div>
+    <asp:GridView runat="server" ID="grdView" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+        <EditRowStyle BackColor="#2461BF" />
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </div>
+        </asp:Content>
+    
---
+++ userPages/sentMessages.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class userPages_sentMessages : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["ID"] == null)
+            Response.Redirect("../Login.aspx");
+        else
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
+                {
+                    con.Open();
+                    string recipientIDs;
+                    int templateID;
+                    string recipientNames;
+                    DataTable table = new DataTable();
+                    table.Columns.Add("body");
+                    table.Columns.Add("TemplateName");
+                    table.Columns.Add("Recipients");
+                    SqlCommand getSentMailsInfo = new SqlCommand("select * from tblSentMails", con);
+                    using (SqlDataReader rdSentMails = getSentMailsInfo.ExecuteReader())
+                    {
+                        while (rdSentMails.Read())
+                        {
+                            recipientNames = "";
+                            using (SqlConnection con3 = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
+                            {
+                                con3.Open();
+                                recipientIDs = rdSentMails["recipientID"].ToString();
+                                templateID = Convert.ToInt32(rdSentMails["templateID"]);
+                                if (Convert.ToInt32(Session["ID"]) == getUserIDbyrecipientID(recipientIDs[0]))//checking if a particular mail is sent by the current user or not
+                                {
+                                    SqlCommand getTemplateName = new SqlCommand("select name from tblTemplates where ID='" + templateID + "' ", con3);
+                                    string templateName = getTemplateName.ExecuteScalar().ToString();
+                                    foreach (char ID in recipientIDs)
+                                    {
+                                        if (ID != ',')
+                                        {
+                                            SqlCommand getRecipientName = new SqlCommand("select name from tblRecipients where ID='" + ID + "'", con3);
+                                            string recipientName = getRecipientName.ExecuteScalar().ToString();
+                                            recipientNames += recipientName;
+                                            recipientNames += ",";
+                                        }
+                                    }
+                                    DataRow dataRow = table.NewRow();
+                                    dataRow["body"] = rdSentMails["body"];
+                                    dataRow["TemplateName"] = templateName;
+                                    dataRow["Recipients"] = recipientNames;
+                                    table.Rows.Add(dataRow);
+                                }
+                            }
+                        }
+                        grdView.DataSource = table;
+                        grdView.DataBind();
+                    }
+
+                }
+
+            }
+        }
+    }
+    public int getUserIDbyrecipientID(char recipientID)
+    {
+        using (SqlConnection con2 = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString))
+        {
+            con2.Open();
+            SqlCommand getCategoryID = new SqlCommand("select categoryID from tblRecipients where ID='" + recipientID + "'", con2);
+            int categoryID = Convert.ToInt32(getCategoryID.ExecuteScalar());
+            SqlCommand getUserID = new SqlCommand("select userID from tblCategory where ID='" + categoryID + "'", con2);
+            int userID = Convert.ToInt32(getUserID.ExecuteScalar());
+            return userID;
+
+        }
+    }
+
+}
